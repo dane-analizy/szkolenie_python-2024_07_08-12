01c2caeb85267d2ad804656e0c3966055a6e7d62
Łukasz Prokulski <prokulski@gmail.com>
2024-07-08 10:33:36 +0200

    dzien 01

---
+++ dzien_01/main.py
@@ -0,0 +1,34 @@
+# print("hello world")
+
+
+# PEP8 - standard pisania kodu
+# https://analityk.edu.pl/jak-pisac-kod-w-python-aby-byl-czytelny-pep8/
+
+
+# mojaZmiennaTypuString - camelCase
+# moja_zmienna_typu_sting - snake_case <- pythonowa składnia
+
+# def funkcja():
+#     """Moja funkcja"""
+#     pass
+
+# for x in lista:
+#     wgwg
+#     wrgwrg
+
+#     # wrhwrhwrh
+#     whwrh
+#     whilewhwrh
+
+#     wrhwrh
+
+
+# funkcja()
+
+# range()
+
+# a = 1 + 2
+# print(a)
+
+
+# Lintery: autopep8, pylint, black, ruff
