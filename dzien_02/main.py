4724c03601764becdae3f25222bdc144c5a1a745
Łukasz Prokulski <prokulski@gmail.com>
2024-07-09 10:27:24 +0200

    dzien 02

--- dzien_02/main.py
+++ dzien_02/main.py
@@ -4,47 +4,47 @@ print("\033c", end="")
 ### ===========
 
 # lista to kolekcja elementów, może by kolekcją różnych typów
-lista = [1, "abc", "z", 123.45]
-# print(lista)
+# lista = [1, "abc", "z", 123.45]
+# # print(lista)
 
-# lista 1-elementowa
-lista_jeden_elem = [1]
-# print(lista_jeden_elem)
+# # lista 1-elementowa
+# lista_jeden_elem = [1]
+# # print(lista_jeden_elem)
 
-zmienna = 123
-lista_ze_zmienna = [1, 2, zmienna]
+# zmienna = 123
+# lista_ze_zmienna = [1, 2, zmienna]
 # print(lista_ze_zmienna)
-zmienna = 567
+# zmienna = 567
 # print(lista_ze_zmienna)
 
 # lista może zawierac inne listy
-lista_list = [
-    lista,
-    lista_jeden_elem,
-    zmienna,
-    "jestem wpisana z ręki",
-    lista_ze_zmienna,
-    lista,
-    lista_jeden_elem,
-    zmienna,
-    "jestem wpisana z ręki",
-    lista_ze_zmienna,
-    lista,
-    lista_jeden_elem,
-    zmienna,
-    "jestem wpisana z ręki",
-    lista_ze_zmienna,
-]
+# lista_list = [
+#     lista,
+#     lista_jeden_elem,
+#     zmienna,
+#     "jestem wpisana z ręki",
+#     lista_ze_zmienna,
+#     lista,
+#     lista_jeden_elem,
+#     zmienna,
+#     "jestem wpisana z ręki",
+#     lista_ze_zmienna,
+#     lista,
+#     lista_jeden_elem,
+#     zmienna,
+#     "jestem wpisana z ręki",
+#     lista_ze_zmienna,
+# ]
 # print(lista_list)
 
 # iteracja po liście
 # for element in lista_list:
 #     print(element)
 
-for licznik, element in enumerate(lista_list):
-    print(licznik, element)
-    if 2 < licznik < 4:
-        print("coś robię")
+# for licznik, element in enumerate(lista_list):
+#     print(licznik, element)
+#     if 2 < licznik < 4:
+#         print("coś robię")
 
 
 # # konkretny element z listy - indeks liczony od 0 włącznie, tu bierzemy indeks 2
@@ -71,15 +71,31 @@ for licznik, element in enumerate(lista_list):
 # print()
 # print(lista_list[::-1])
 
-# czy lista jest pusta
-pusta_lista = []
-if pusta_lista:
-    print("jestem NIEPUSTĄ listą")
-else:
-    print("jestem PUSTĄ listą")
+# # czy lista jest pusta
+# pusta_lista = []
+# if pusta_lista:
+#     print("jestem NIEPUSTĄ listą")
+# else:
+#     print("jestem PUSTĄ listą")
 
-niepusta_lista = []
-if niepusta_lista:
-    print("jestem NIEPUSTĄ listą")
-else:
-    print("jestem PUSTĄ listą")
+# niepusta_lista = []
+# if niepusta_lista:
+#     print("jestem NIEPUSTĄ listą")
+# else:
+#     print("jestem PUSTĄ listą")
+
+
+# jak stworzyc pusta liste i dodac do niej elementy?
+# lista = []
+
+# lista.append("abc")
+# print(lista)
+
+# lista.append(123)
+# print(lista)
+
+
+### ZDANIE
+
+# Napisz kod który umieści w nowej liście 10 kolejnych potęg liczby 2.
+# Następnie przeiteruj po tej liście (for-em) i każdy z jej elementów wyświetl na konsoli w osobnej linii.
