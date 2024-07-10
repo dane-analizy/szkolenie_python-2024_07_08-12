f44056d7ebff8facc365bd59c8b3951d12ff765f
Łukasz Prokulski <prokulski@gmail.com>
2024-07-10 09:39:21 +0200

    dzien 3

---
+++ dzien_03/main.py
@@ -0,0 +1,20 @@
+# czyszczenie terminala, odpowiednik Ctrl-L
+print("\033c", end="")
+
+### ===========
+
+# napis = "abc"
+# print(napis * 5)
+
+# lista = [1, 2, 3]
+# print(lista * 5)
+
+# import numpy as np
+
+# wektor = np.array(lista)
+# print(wektor, type(wektor))
+# print(wektor * 4)
+# print(wektor + np.array([3, 2, 1]))
+
+# print("=" * 80)
+
