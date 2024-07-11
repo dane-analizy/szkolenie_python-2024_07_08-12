ce2597f1177799ed3f1013b0f695145c731b152c
Łukasz Prokulski <prokulski@gmail.com>
2024-07-11 09:46:38 +0200

    dzien 4

--- dzien_04/main.py
+++ dzien_04/main.py
@@ -9,3 +9,44 @@ print("\033c", end="")
 ### ZADANIE
 
 # Wyświetl wynik dzielenia jedynki przez kolejne liczby z zakresu od -10 do 10.
+
+
+# rozwiązanie - zwróci błąd przy dzieleniu przez 0 i program przerwie swoje działanie
+
+# for i in range(-10, 10):
+#     wynik = 1 / i
+#     print(wynik)
+
+
+# obsługa błędów - kontrukcja try - except
+
+# for i in range(-10, 10):
+#     try:
+#         wynik = 1 / i
+#     except Exception as e: # każdy rodzaj błędu
+#         print(f"nie mogę dzielić przez {i}, błąd {e}")
+#     print(wynik)
+
+
+# różne typy wyjątków:
+# for i in [-2, -1, 0, "a", 1, 2]:
+#     try:
+#         wynik = 1 / i
+#     except Exception as e: # każdy rodzaj błędu
+#         print(f"nie mogę dzielić przez {i}, błąd {e}")
+#     print(wynik)
+
+# print(1/0)
+# print(1/"a")
+
+# for i in [-2, -1, 0, "a", (1, 2, "4"), 1, 2, {"k": 120, "v": 30}]:
+#     try:
+#         wynik = 1 / i
+#     except ZeroDivisionError as e:
+#         print(f"nie mogę dzielić przez zero, błąd {e}")
+#         # wiecej czynnosci
+#     except TypeError as e:
+#         print(f"nie mogę dzielić przez ten typ danych ({i}), błąd {e}")
+#     except Exception:
+#         print("inny błąd dzielenia, błąd {e}")
+#     print(wynik)
