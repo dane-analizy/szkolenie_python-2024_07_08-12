e4a59d6ad0926cec72a29f02ceb07f70aec4b08f
Łukasz Prokulski <prokulski@gmail.com>
2024-07-11 11:39:18 +0200

    dzien 4

--- dzien_04/main.py
+++ dzien_04/main.py
@@ -241,4 +241,79 @@ print("\033c", end="")
 #   - pusta linia nie trafia do listy
 
 
+# def wez_nazwe_pliku():
+#     return input("Podaj nazwe pliku: ")
+
+
+# def wczytaj_linie(nazwa_pliku, enc):
+#     return [
+#         linia.strip()
+#         for linia in open(nazwa_pliku, "r", encoding=enc)
+#         if len(linia.strip())
+#     ]
+
+
+# def podziel_linie(lista_linii, separator):
+#     return [tuple(linia.split(separator)) for linia in lista_linii]
+
+
+# def daj_czyste_dane_z_pliku():
+#     enc = "utf-8"
+#     sep = ";"
+
+#     plik = wez_nazwe_pliku()
+#     print(f"Krok 1:\n{plik=}")
+
+#     dane = wczytaj_linie(plik, enc)
+#     print("Krok 2:")
+#     print(dane)
+
+#     dane_czyste = podziel_linie(dane, sep)
+#     print("Krok 3:")
+#     print(dane_czyste)
+
+#     print("Funkcja się skończyła:")
+
+#     return dane_czyste
+
+
+# print("Zaraz wywołam funkcję")
+# dane = daj_czyste_dane_z_pliku()
+# print("Wynik z wywołania funkcji:")
+# print(dane)
+
+
+# parametry domyślne
+
+
+def wez_nazwe_pliku():
+    return input("Podaj nazwe pliku: ")
+
+
+def wczytaj_linie(nazwa_pliku, enc):
+    return [
+        linia.strip()
+        for linia in open(nazwa_pliku, "r", encoding=enc)
+        if len(linia.strip())
+    ]
+
+
+def podziel_linie(lista_linii, separator):
+    return [tuple(linia.split(separator)) for linia in lista_linii]
+
+
+def daj_czyste_dane_z_pliku(nazwa=None, enc="utf-8", sep=";"):
+    if not nazwa:
+        # parametr "nazwa" nie został podany, więc zapytaj użytkownika
+        nazwa = wez_nazwe_pliku()
+    # czy plik o nazwie zawartej w zmiennej "nazwa" istnieje? -> zagadnienie z plików
+    dane = wczytaj_linie(nazwa, enc)
+    dane_czyste = podziel_linie(dane, sep)
+    return dane_czyste
+
+
+dane = daj_czyste_dane_z_pliku()
+print(dane)
+
+
 # pliki i katalogi - przeglądanie
