44976e77c0c0e771a47d49fb978fc88598062b40
Łukasz Prokulski <prokulski@gmail.com>
2024-07-11 09:35:22 +0200

    dzien 4

---
+++ dzien_04/main.py
@@ -0,0 +1,11 @@
+# czyszczenie terminala, odpowiednik Ctrl-L
+print("\033c", end="")
+
+### ===========
+
+
+# Wyjątki
+
+### ZADANIE
+
+# Wyświetl wynik dzielenia jedynki przez kolejne liczby z zakresu od -10 do 10.
