ee02e87c2e5a78118d718c47ea8462daa52ffd8b
Łukasz Prokulski <prokulski@gmail.com>
2024-07-12 14:16:03 +0200

    dzien 5

--- dzien_05_flask/tabelka.py
+++ dzien_05_flask/tabelka.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-lista = [f"element {i:02d} w kolejności" for i in range(1, 100)]
+lista = [f"element {i:02d} w kolejności" for i in range(1, 5)]
 zmienna = "ala ma kota"
 
 
@@ -12,5 +12,25 @@ def home():
     return render_template("tabelka.html", nasza_lista=lista, napis=zmienna)
 
 
+def dodawanie(a, b):
+    return a + b
+
+
+@app.route("/add/<a>/<b>")
+def dodaj(a, b):
+    a = float(a)
+    b = float(b)
+
+    return {"a": a, "b": b, "suma": dodawanie(a, b)}
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+### ZADANIE
+
+# Napisz "endpoint" we Flasku, który:
+# - przyjmie w adresie 2 liczby
+# - policzy ich iloczyn
+# - wyświetli liczby oraz wynik korzystając z nowej templatki
