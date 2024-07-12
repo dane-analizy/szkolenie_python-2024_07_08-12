d95b0ceb3f3c1b8f1dab0c37a5e9d0bb56fef410
Łukasz Prokulski <prokulski@gmail.com>
2024-07-12 13:20:52 +0200

    dzien 5

---
+++ dzien_05_flask/app.py
@@ -0,0 +1,22 @@
+# pip install Flask
+
+# polecany tutorial https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/index")
+def home():
+    return "Jestem stroną główną"
+
+
+@app.route("/nowa_strona")
+def nowa_strona():
+    return "Jestem sobie nową stroną"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
