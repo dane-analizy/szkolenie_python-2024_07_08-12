c64ea14d7d01dac13602d7c9d9a222849ee28258
Łukasz Prokulski <prokulski@gmail.com>
2024-07-12 11:54:47 +0200

    dzien 5

--- nbp_db/main.py
+++ nbp_db/main.py
@@ -2,3 +2,11 @@ from utils.config import get_config
 from utils.db import generate_connection_string
 from utils.nbp import get_month_rates
 
+# wczytanie danych z NBP do listy słowników
+# zapisanie listy słowników do bazy danych:
+# 1. połączenie do bazy
+# 1a. zbudowanie connection stringa
+# 1a'. pobranie konfiguracji
+# 1b. podłączenie -> db_connection
+# 2. zapisanie samych danych
+# 3. odłączenie się od bazy
