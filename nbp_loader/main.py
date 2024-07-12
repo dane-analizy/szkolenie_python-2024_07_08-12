796e6ef92328efeb854bb632f4bda8322acdae03
Łukasz Prokulski <prokulski@gmail.com>
2024-07-12 11:46:34 +0200

    dzien 5

--- nbp_loader/main.py
+++ nbp_loader/main.py
@@ -2,10 +2,8 @@ import requests
 
 JAKIE_WALUTY = ["GBP", "USD", "EUR", "CHF"]
 
-print("\033c", end="")
 
-
-def get_nbp_table(dzien, miesiac=6, rok=2024):
+def get_nbp_table(dzien, miesiac, rok):
     data = f"{rok}-{miesiac:02d}-{dzien:02d}"
     url = f"https://api.nbp.pl/api/exchangerates/tables/a/{data}/?format=json"
 
@@ -32,11 +30,29 @@ def get_nbp_table(dzien, miesiac=6, rok=2024):
     return notowania
 
 
-lista_notowan = []
-for dzien in range(1, 31):
-    notowanie = get_nbp_table(dzien=dzien)
-    if not notowanie["error"]:
-        lista_notowan.append(notowanie)
+def get_month_rates(miesiac=6, rok=2024):
+    lista_notowan = []
+    for dzien in range(1, 31):
+        notowanie = get_nbp_table(dzien=dzien, miesiac=miesiac, rok=rok)
+        if not notowanie["error"]:
+            lista_notowan.append(notowanie)
+
+    return lista_notowan
 
 
-print(lista_notowan)
+def wypisz_notowania(lista):
+    for notowanie in lista:
+        print(f"{notowanie['data']}:")
+        for waluta in sorted(JAKIE_WALUTY):
+            print(f"\t{waluta} = {notowanie[waluta]}")
+
+
+def main():
+    print("\033c", end="")
+
+    lista_notowan = get_month_rates(6, 2024)
+    wypisz_notowania(lista_notowan)
+
+
+if __name__ == "__main__":
+    main()
